150014c0ce05cba5f74680ee7da7f475b327c323
Ivaylo Nanchev <tolkin.in@gmail.com>
2023-09-26 13:16:12 +0300

    Added StudentInfo

---
+++ StudentInfo/Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+namespace Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double pricePerCapsule = double.Parse(Console.ReadLine());
+            int daysInMonth = int.Parse(Console.ReadLine());
+            int capsulesCount = int.Parse(Console.ReadLine());
+            int a = 0;
+            double momentPrice = 0;
+            double totalPrice = 0;
+            while (a < n)
+            {
+                momentPrice+= ((daysInMonth * capsulesCount) * pricePerCapsule);
+                Console.WriteLine($"The price for the coffee is: ${momentPrice:F2}");
+                totalPrice += momentPrice;
+                momentPrice = 0;
+                if (a + 1 < n)
+                {
+                    pricePerCapsule = double.Parse(Console.ReadLine());
+                    daysInMonth = int.Parse(Console.ReadLine());
+                    capsulesCount= int.Parse(Console.ReadLine());
+                }
+                
+                a++;
+            }
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+
+        }
+    }
+}
---
+++ StudentInfo/Rage/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Rage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int games = int.Parse(Console.ReadLine());
+            double gm = Convert.ToDouble(games);
+            double headset = double.Parse(Console.ReadLine());
+            double mouse = double.Parse(Console.ReadLine());
+            double keyboard = double.Parse(Console.ReadLine());
+            double display = double.Parse(Console.ReadLine());
+            double a = 0;
+            if (games >= 2) 
+            { 
+                headset *= Math.Floor(gm / 2);
+                a += headset;
+            }
+            if (games >= 3)
+            {
+                mouse *= Math.Floor(gm / 3);
+                a += mouse;
+            }
+            if (games >= 6)
+            {
+                keyboard *= Math.Floor(gm / 6);
+                a += keyboard;
+            }
+            if (games >= 12)
+            {
+                display *= Math.Floor(gm / 12);
+                a += display;
+            }
+            Console.WriteLine($"Rage expenses: {a:F2} lv.");
+        }
+    }
+}
