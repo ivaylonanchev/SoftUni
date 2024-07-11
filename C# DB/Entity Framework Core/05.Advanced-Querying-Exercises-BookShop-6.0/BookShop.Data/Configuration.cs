e0106d8abeefadcde1f1838c56e59f02b1f7ada6
Ivaylo Nanchev <tolkin.in@gmail.com>
2024-07-11 19:17:28 +0300

    loool

---
+++ C# DB/Entity Framework Core/05.Advanced-Querying-Exercises-BookShop-6.0/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-FE75MLC\\SQLEXPRESS;Database=StudentSystem;Integrated Security=True;";
+    }
+}
