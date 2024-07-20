2e3d3cc99b39bbf34e63c327582d51c7f9b62435
Ivaylo Nanchev <tolkin.in@gmail.com>
2024-07-20 13:11:02 +0300

    .

--- C# DB/Entity Framework Core/07.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/obj/Debug/net6.0/ProductShop.AssemblyInfo.cs
+++ C# DB/Entity Framework Core/07.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/obj/Debug/net6.0/ProductShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b71bf6887871e811c2e40576fa2da3e0e8f4d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7a34166eddd10cfbc5e2791298a7d7ebee766b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
