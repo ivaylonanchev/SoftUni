51ac8a5351e1c33ea6ba91b9c36901cdae821d66
Ivaylo Nanchev <tolkin.in@gmail.com>
2024-07-21 16:43:58 +0300

    sub

---
+++ C# DB/Entity Framework Core/07. Json/07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
@@ -0,0 +1,82 @@
+﻿using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Migrations;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            //string  carsText = File.ReadAllText("../../../Datasets/parts.json");
+            //var user = JsonConvert.DeserializeObject<List<Part>>(carsText);
+            //context.Parts.AddRange(user);
+            //context.SaveChanges();
+
+        }
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var text = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+
+            context.Suppliers.AddRange(text);
+            context.SaveChanges();
+
+            return $"Successfully imported {text.Count}.";
+        }
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var parts = JsonConvert.DeserializeObject<List<Part>>(inputJson);
+
+            var validSupplierIds = context.Suppliers
+                .Select(x => x.Id)
+                .ToArray();
+
+            var partsWithValidSupplierIds = parts
+                .Where(p => validSupplierIds.Contains(p.SupplierId))
+                .ToArray();
+
+            context.Parts.AddRange(partsWithValidSupplierIds);
+            context.SaveChanges();
+
+            return $"Successfully imported {partsWithValidSupplierIds.Length}.";
+        }
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var carsDtos = JsonConvert.DeserializeObject<List<ImportCarDto>>(inputJson);
+
+            var cars = new HashSet<Car>();
+            var partsCars = new HashSet<PartCar>();
+
+            foreach (var carDto in carsDtos)
+            {
+                var newCar = new Car()
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TraveledDistance = carDto.TraveledDistance
+                };
+
+                cars.Add(newCar);
+                foreach (var partId in carDto.PartsId.Distinct())
+                {
+                    partsCars.Add(new PartCar()
+                    {
+                        Car = newCar,
+                        PartId = partId
+                    });
+                }
+            }
+            context.Cars.AddRange(cars);
+            context.PartsCars.AddRange(partsCars);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}.";
+        }
+    }
+}
--- C# DB/Entity Framework Core/Demo/Demo/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿using Microsoft.Data.SqlClient;
-
-string connectionString = "Server=.;Database=SoftUni;IntegratedSecurity=true;TrustServerCertificate=true;";
-SqlConnection connection = new SqlConnection(connectionString); 
